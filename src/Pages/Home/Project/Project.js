b591e479a0e3f01e451995098c9d6d8826a6cdda
ashimbhowmik <ashimbhowmik239@gmail.com>
2022-12-10 15:58:52 +0600

    [Basic set-up]

---
+++ src/Pages/Home/Project/Project.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Project = () => {
+  return (
+    <div>
+      <h1>fdgk</h1>
+    </div>
+  );
+};
+
+export default Project;
---
+++ src/Routes/Routes.js
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+
+import Error from "../Pages/Error/Error";
+import Home from "../Pages/Home/Home";
+
+export const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    errorElement: <Error></Error>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
