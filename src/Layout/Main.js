1511eb29803fe671523d34f2326172a7238456d0
ashimbhowmik <ashimbhowmik239@gmail.com>
2022-12-10 18:18:55 +0600

    [Home 1st part]

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -5,7 +5,7 @@ import Navber from "../Pages/Shared/Navber/Navber";
 
 const Main = () => {
   return (
-    <div className="lg:w-[90%] mx-auto">
+    <div className="lg:w-[90%] mx-auto lg:mt-3">
       <Navber></Navber>
       <Outlet></Outlet>
       <Footer></Footer>
